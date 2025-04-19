b2fae005e9760046782cc81776403f3e5874602e
Het Tank <hettank34@gamil.com>
2025-04-19 16:23:00 +0530

    Add start script to package.json and simplify MongoDB connection in app.js

--- src/app.js
+++ src/app.js
@@ -21,13 +21,10 @@ app.get('/api/v1/test', (req, res) => {
 app.use('/api/v1', routes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running...');
+      console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
